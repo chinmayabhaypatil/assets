47288f4935e9adfaf40edddcbd0a9a7d57e2e094
chinupatil <43513157+chinupatil@users.noreply.github.com>
2018-09-23 19:55:30 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="shortcut icon" type="image/x-icon" href="logo.png"/>
+	<title>Welcome to Assets Management system</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="stylinglogin.css">
+
+	
+
+<script type="text/javascript" src="https://ff.kis.v2.scr.kaspersky-labs.com/0CC2FFFD-4955-E041-8315-971B70348EA6/main.js" charset="UTF-8"></script></head>
+<body>
+<form method="post">
+
+	<header>
+
+		<p style="font-size: 35px;color: #0340FD;"><img style="width: 140px;height: 140px;padding-left: 240px;" src="logo.png" align="middle"> Welcome to Assets Management System!</p>
+
+	</header>
+
+
+	
+	<section style="background:rgba(255,255,255,0.5);">
+		<marquee scrollamount="3" scrolldelay="10" style="font-size: 20px;color: #000000">&diams;Welcome!</marquee>
+	</section>
+	
+
+		<br><br>
+
+		<section class="back" style="font-size: 23px;padding-top: 10px;padding-bottom: 10px;top:700px;">Kindly Login to continue.</section>
+
+		<section class="back" style="font-size: 20px;">
+			
+
+			<table>
+
+				<tr>
+
+					<td><label for="email">Email-ID:</label></td>
+					<td><input type="text" name="id" size="20" placeholder="Enter Your ID" autocomplete="off" autofocus="on"></td>
+
+				</tr>
+
+				<tr>
+
+					<td><label for="pass">Password:</label></td>
+					<td><input type="Password" name="pass" size="20" placeholder="Your Password" autocomplete="off"></td>
+
+				</tr>
+
+			</table>
+
+			<button type="submit" class="button btn">Login</button>
+			
+		</section>	
+
+	</section>
+
+</form>
+</body>
+</html>
